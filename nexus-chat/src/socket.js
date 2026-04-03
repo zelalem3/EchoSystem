2cfc1d1b5696a640f2e8f7f090117060b12945ec
zelalem3 <zgetnet24@gmail.com>
2026-04-03 12:52:05 +0300

    fix: cors error and route updated for backend

--- nexus-chat/src/socket.js
+++ nexus-chat/src/socket.js
@@ -2,15 +2,14 @@
 import { io } from "socket.io-client";
 
 // Replace with your server URL
-const SERVER_URL = "http://localhost:5000";  // Adjust this to match your server's URL
-
+const SERVER_URL = "http://localhost:5000 || https://echosystem-1.onrender.com"; 
 // Create a socket instance
 export const socket = io(SERVER_URL, {
   // Options (optional)
-  // autoConnect: false,  // Set to true (default) so the socket connects immediately
+  // autoConnect: false,  
 });
 
-// Export additional socket events if needed
+
 export const connectSocket = () => {
   socket.connect();
 };
@@ -19,7 +18,7 @@ export const disconnectSocket = () => {
   socket.disconnect();
 };
 
-// Optionally handle socket events (like connection or disconnection events)
+
 socket.on("connect", () => {
   console.log("Socket connected:", socket.id);
 });
